074b2c4170d7006413e512b9fb06a87c0d6892dc
f23aa005-oike <f23aa005@chuo.ac.jp>
2025-06-18 16:04:15 +0900

    ファイル

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,56 +1,128 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  final title = 'Flutterサンプル';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(
-        title:this.title,
+      title: 'Generated App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff2196f3),
+        canvasColor: const Color(0xfffafafa),
       ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title}): super();
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _message = 'Hello!';
-
-  void _setMessage() {
-    setState(() {
-      _message = 'タップしました！';
-    });
-  }
+  final _controller = TextEditingController();
+  final _fname = 'flutter_sampledata.txt';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Home'),
       ),
-      body: Text(
-        _message,
-        style: TextStyle(fontSize:32.0),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child:Column(
+          children:<Widget>[
+            Text('FILE ACCESS.',
+              style: TextStyle(fontSize: 32,
+                fontWeight: ui.FontWeight.w500),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            TextField(
+              controller: _controller,
+              style: TextStyle(fontSize: 24),
+              minLines: 1,
+              maxLines: 5,
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setMessage,
-        tooltip: 'set message.',
-        child: Icon(Icons.star_outlined),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Save',
+            icon: Icon(Icons.save, color: Colors.white, size:32),
+          ),
+          BottomNavigationBarItem(
+            label: 'Load',
+            icon: Icon(Icons.open_in_new, color: Colors.white, size:32),
+          ),
+        ],
+        onTap: (int value) async {
+          switch (value) {
+            case 0:
+              saveIt(_controller.text);
+              setState(() {
+                _controller.text = '';
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: Text("saved!"),
+                  content: Text("save message to file."),
+                )
+              );
+              break;
+            case 1:
+            String value =  await loadIt();
+            setState(() {
+              _controller.text = value;
+            });
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: Text("loaded!"),
+                content: Text("load message from file."),
+              )
+            );
+            break;
+            default:
+              print('no defalut.');
+          }
+        },
       ),
     );
+  }
+
+  Future<File> getDataFile(String filename) async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File(directory.path + '/' + filename);
+  }
+
+  void saveIt(String value) async {
+    final file = await getDataFile(_fname);
+    file.writeAsString(value);
+  }
+
+  Future<String> loadIt() async {
+    try {
+      final file = await getDataFile(_fname);
+      return file.readAsString();
+    } catch (e) {
+      return '*** no data ***';
+    }
   }
 }
